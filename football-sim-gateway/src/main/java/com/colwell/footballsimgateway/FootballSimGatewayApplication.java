0aa8fa637c7c8242631a68eefecd2cebfd99b206
colwell-admin <colwell.russell.emerson@gmail.com>
2022-02-07 13:58:21 -0500

    Added Gateway

---
+++ football-sim-gateway/src/main/java/com/colwell/footballsimgateway/FootballSimGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.colwell.footballsimgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballSimGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballSimGatewayApplication.class, args);
+	}
+
+}
---
+++ football-sim-gateway/src/test/java/com/colwell/footballsimgateway/FootballSimGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colwell.footballsimgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballSimGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
