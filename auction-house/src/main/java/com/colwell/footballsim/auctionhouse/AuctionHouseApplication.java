87f583ca30dd595db654d2843bbd18ec90620e43
colwell-russell <colwell.russell.emerson@gmail.com>
2022-02-21 14:32:23 -0500

    Added keystone auth server.. need to fix auction house 401

--- auction-house/src/main/java/com/colwell/footballsim/auctionhouse/AuctionHouseApplication.java
+++ auction-house/src/main/java/com/colwell/footballsim/auctionhouse/AuctionHouseApplication.java
@@ -3,9 +3,11 @@ package com.colwell.footballsim.auctionhouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableWebFluxSecurity
 public class AuctionHouseApplication {
 
 	public static void main(String[] args) {
