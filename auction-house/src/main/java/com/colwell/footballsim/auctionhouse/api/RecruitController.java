96c653370e5e763218ec60b70a2befb08f6dca1e
colwell-russell <colwell.russell.emerson@gmail.com>
2022-02-27 14:37:50 -0500

    Added test to veriy user name or id is captured in spring security

--- auction-house/src/main/java/com/colwell/footballsim/auctionhouse/api/RecruitController.java
+++ auction-house/src/main/java/com/colwell/footballsim/auctionhouse/api/RecruitController.java
@@ -4,11 +4,15 @@ import com.colwell.footballsim.auctionhouse.entity.Recruit;
 import com.colwell.footballsim.auctionhouse.service.RecruitService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/v1/recruit")
 public class RecruitController {
@@ -17,7 +21,8 @@ public class RecruitController {
     private RecruitService recruitService;
 
     @GetMapping("/all")
-    public Iterable<Recruit> getAvailableRecruits(){
+    public Iterable<Recruit> getAvailableRecruits(Authentication authentication){
+        log.info("User Name: " + authentication.getName());
         return recruitService.getAvailableRecruits();
     }
 
