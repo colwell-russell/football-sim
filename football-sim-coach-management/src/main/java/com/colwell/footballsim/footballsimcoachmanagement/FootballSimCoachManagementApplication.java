19d495e01c644912cc4df93a4c14154f59e856c0
colwell-admin <colwell.russell.emerson@gmail.com>
2022-03-09 08:56:19 -0500

    Created Micro for coach management

---
+++ football-sim-coach-management/src/main/java/com/colwell/footballsim/footballsimcoachmanagement/FootballSimCoachManagementApplication.java
@@ -0,0 +1,13 @@
+package com.colwell.footballsim.footballsimcoachmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballSimCoachManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballSimCoachManagementApplication.class, args);
+	}
+
+}
---
+++ football-sim-coach-management/src/test/java/com/colwell/footballsim/footballsimcoachmanagement/FootballSimCoachManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colwell.footballsim.footballsimcoachmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballSimCoachManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
